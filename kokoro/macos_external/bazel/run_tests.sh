2ede8db94443aaa95bc65aeb51444347255cf286
Moreno Ambrosin <ambrosin@google.com>
2022-08-23 13:20:34 -0700

    Add kokoro test script for MacOS
    PiperOrigin-RevId: 469539984
    Change-Id: I5d8a1b5e2161def5770bfdedeebb84f13b43f31a

---
+++ kokoro/macos_external/bazel/run_tests.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+################################################################################
+
+set -euo pipefail
+
+export XCODE_VERSION=11.3
+export DEVELOPER_DIR="/Applications/Xcode_${XCODE_VERSION}.app/Contents/Developer"
+export ANDROID_HOME="/Users/kbuilder/Library/Android/sdk"
+export COURSIER_OPTS="-Djava.net.preferIPv6Addresses=true"
+
+if [[ -n "${KOKORO_ROOT:-}" ]] ; then
+  cd "${KOKORO_ARTIFACTS_DIR}/git/tink_java_apps"
+  use_bazel.sh "$(cat .bazelversion)"
+fi
+
+: "${TINK_BASE_DIR:=$(cd .. && pwd)}"
+
+# Check for dependencies in TINK_BASE_DIR. Any that aren't present will be
+# downloaded.
+readonly GITHUB_ORG="https://github.com/tink-crypto"
+./kokoro/testutils/fetch_git_repo_if_not_present.sh "${TINK_BASE_DIR}" \
+  "${GITHUB_ORG}/tink-java"
+
+./kokoro/testutils/update_android_sdk.sh
+cp "WORKSPACE" "WORKSPACE.bak"
+./kokoro/testutils/replace_http_archive_with_local_repository.py \
+  -f "WORKSPACE" \
+  -t "${TINK_BASE_DIR}"
+./kokoro/testutils/run_bazel_tests.sh .
+mv "WORKSPACE.bak" "WORKSPACE"
