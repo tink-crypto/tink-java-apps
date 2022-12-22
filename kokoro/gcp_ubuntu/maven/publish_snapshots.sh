f74d897428d7cc052b7f1ec790e9cf261cb8e978
Moreno Ambrosin <ambrosin@google.com>
2022-12-22 04:53:04 -0800

    Add missing call to replace_http_archive_with_local_repository.py in snapshot publishing script.
    PiperOrigin-RevId: 497136752
    Change-Id: If1b804296531a051be2c3dfceb20cdc8e3c76913

--- kokoro/gcp_ubuntu/maven/publish_snapshots.sh
+++ kokoro/gcp_ubuntu/maven/publish_snapshots.sh
@@ -25,22 +25,29 @@ readonly IS_KOKORO
 if [[ "${IS_KOKORO}" == "true" ]] ; then
   TINK_BASE_DIR="$(echo "${KOKORO_ARTIFACTS_DIR}"/git*)"
   cd "${TINK_BASE_DIR}/tink_java_apps"
+fi
+
+./kokoro/testutils/replace_http_archive_with_local_repository.py \
+  -f "WORKSPACE" -t "${TINK_BASE_DIR}"
+
+# TODO(b/263465812): Add test example app for testing the Maven snapshots.
+
+if [[ "${IS_KOKORO}" == "true" \
+      && "${KOKORO_JOB_NAME}" == "${GITHUB_JOB_NAME}" ]] ; then
   readonly GITHUB_JOB_NAME="tink/github/java_apps/gcp_ubuntu/maven/continuous"
-  if [[ "${KOKORO_JOB_NAME}" == "${GITHUB_JOB_NAME}" ]]; then
-    # GITHUB_ACCESS_TOKEN is populated by Kokoro.
-    readonly GIT_CREDENTIALS="ise-crypto:${GITHUB_ACCESS_TOKEN}"
-    readonly GITHUB_URL="https://${GIT_CREDENTIALS}@github.com/tink-crypto/tink-java-awskms.git"
+  # GITHUB_ACCESS_TOKEN is populated by Kokoro.
+  readonly GIT_CREDENTIALS="ise-crypto:${GITHUB_ACCESS_TOKEN}"
+  readonly GITHUB_URL="https://${GIT_CREDENTIALS}@github.com/tink-crypto/tink-java-apps.git"
 
-    ./maven/maven_deploy_library.sh -u "${GITHUB_URL}" \
-      -n paymentmethodtoken/maven snapshot apps-paymentmethodtoken \
-      maven/tink-java-apps-paymentmethodtoken.pom.xml HEAD
+  ./maven/maven_deploy_library.sh -u "${GITHUB_URL}" \
+    -n paymentmethodtoken/maven snapshot apps-paymentmethodtoken \
+    maven/tink-java-apps-paymentmethodtoken.pom.xml HEAD
 
-    ./maven/maven_deploy_library.sh -u "${GITHUB_URL}" \
-      -n rewardedads/maven snapshot apps-rewardedads \
-      maven/tink-java-apps-rewardedads.pom.xml HEAD
+  ./maven/maven_deploy_library.sh -u "${GITHUB_URL}" \
+    -n rewardedads/maven snapshot apps-rewardedads \
+    maven/tink-java-apps-rewardedads.pom.xml HEAD
 
-    ./maven/maven_deploy_library.sh -u "${GITHUB_URL}" \
-      -n webpush/maven snapshot apps-webpush \
-      maven/tink-java-apps-webpush.pom.xml HEAD
-  fi
+  ./maven/maven_deploy_library.sh -u "${GITHUB_URL}" \
+    -n webpush/maven snapshot apps-webpush \
+    maven/tink-java-apps-webpush.pom.xml HEAD
 fi
