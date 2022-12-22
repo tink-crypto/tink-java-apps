8ffe50f036f180cb4e8cef8e43ede95ed060ef8d
Moreno Ambrosin <ambrosin@google.com>
2022-12-22 02:19:49 -0800

    Add continuous job to publish a Maven snapshot
    PiperOrigin-RevId: 497115205
    Change-Id: I4ea743c8c6e0062becdb18d74a569d5409122159

---
+++ kokoro/gcp_ubuntu/maven/run_test.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+################################################################################
+
+set -euo pipefail
+
+IS_KOKORO="false"
+if [[ -n "${KOKORO_ARTIFACTS_DIR:-}" ]] ; then
+  IS_KOKORO="true"
+fi
+readonly IS_KOKORO
+
+if [[ "${IS_KOKORO}" == "true" ]] ; then
+  TINK_BASE_DIR="$(echo "${KOKORO_ARTIFACTS_DIR}"/git*)"
+  cd "${TINK_BASE_DIR}/tink_java_apps"
+  readonly GITHUB_JOB_NAME="tink/github/java_apps/gcp_ubuntu/maven/continuous"
+  if [[ "${KOKORO_JOB_NAME}" == "${GITHUB_JOB_NAME}" ]]; then
+    # GITHUB_ACCESS_TOKEN is populated by Kokoro.
+    readonly GIT_CREDENTIALS="ise-crypto:${GITHUB_ACCESS_TOKEN}"
+    readonly GITHUB_URL="https://${GIT_CREDENTIALS}@github.com/tink-crypto/tink-java-awskms.git"
+
+    ./maven/maven_deploy_library.sh -u "${GITHUB_URL}" \
+      -n paymentmethodtoken/maven snapshot apps-paymentmethodtoken \
+      maven/tink-java-apps-paymentmethodtoken.pom.xml HEAD
+
+    ./maven/maven_deploy_library.sh -u "${GITHUB_URL}" \
+      -n rewardedads/maven snapshot apps-rewardedads \
+      maven/tink-java-apps-rewardedads.pom.xml HEAD
+
+    ./maven/maven_deploy_library.sh -u "${GITHUB_URL}" \
+      -n webpush/maven snapshot apps-webpush \
+      maven/tink-java-apps-webpush.pom.xml HEAD
+  fi
+fi
