cb18a347db3455a4eadd0ace09452a7b3cf73d38
Moreno Ambrosin <ambrosin@google.com>
2023-07-06 04:12:27 -0700

    Run tink-java-apps Kokoro jobs on containers
    PiperOrigin-RevId: 545943339
    Change-Id: I2559d067bb1bef168412e8b4c2e65b1feabf835f

--- kokoro/gcp_ubuntu/bazel/run_tests.sh
+++ kokoro/gcp_ubuntu/bazel/run_tests.sh
@@ -14,14 +14,35 @@
 # limitations under the License.
 ################################################################################
 
-set -euo pipefail
+# By default when run locally this script runs the command below directly on the
+# host. The CONTAINER_IMAGE variable can be set to run on a custom container
+# image for local testing. E.g.:
+#
+# CONTAINER_IMAGE="us-docker.pkg.dev/tink-test-infrastructure/tink-ci-images/linux-tink-java-base:latest" \
+#  sh ./kokoro/gcp_ubuntu/bazel/run_tests.sh
+#
+# The user may specify TINK_BASE_DIR as the folder where to look for
+# tink-java-awskms and its depndencies. That is:
+#   ${TINK_BASE_DIR}/tink_java
+#   ${TINK_BASE_DIR}/tink_java_apps
+set -eEuo pipefail
 
+RUN_COMMAND_ARGS=()
 if [[ -n "${KOKORO_ROOT:-}" ]] ; then
   TINK_BASE_DIR="$(echo "${KOKORO_ARTIFACTS_DIR}"/git*)"
   cd "${TINK_BASE_DIR}/tink_java_apps"
+  source "./kokoro/testutils/tink_test_container_images.sh"
+  CONTAINER_IMAGE="${TINK_JAVA_BASE_IMAGE}"
+  RUN_COMMAND_ARGS+=( -k "${TINK_GCR_SERVICE_KEY}" )
 fi
-
 : "${TINK_BASE_DIR:=$(cd .. && pwd)}"
+readonly TINK_BASE_DIR
+readonly CONTAINER_IMAGE
+
+if [[ -n "${CONTAINER_IMAGE}" ]]; then
+  RUN_COMMAND_ARGS+=( -c "${CONTAINER_IMAGE}" )
+fi
+readonly RUN_COMMAND_ARGS
 
 # Check for dependencies in TINK_BASE_DIR. Any that aren't present will be
 # downloaded.
@@ -31,6 +52,14 @@ readonly GITHUB_ORG="https://github.com/tink-crypto"
 
 cp "WORKSPACE" "WORKSPACE.bak"
 ./kokoro/testutils/replace_http_archive_with_local_repository.py \
-  -f "WORKSPACE" -t "${TINK_BASE_DIR}"
-./kokoro/testutils/run_bazel_tests.sh .
-mv "WORKSPACE.bak" "WORKSPACE"
+  -f "WORKSPACE" -t ..
+
+# Run cleanup on EXIT.
+trap cleanup EXIT
+
+cleanup() {
+  mv WORKSPACE.bak WORKSPACE
+}
+
+./kokoro/testutils/run_command.sh "${RUN_COMMAND_ARGS[@]}" \
+  ./kokoro/testutils/run_bazel_tests.sh .
--- kokoro/gcp_ubuntu/maven/publish_snapshots.sh
+++ kokoro/gcp_ubuntu/maven/publish_snapshots.sh
@@ -14,7 +14,18 @@
 # limitations under the License.
 ################################################################################
 
-set -euo pipefail
+# By default when run locally this script runs the command below directly on the
+# host. The CONTAINER_IMAGE variable can be set to run on a custom container
+# image for local testing. E.g.:
+#
+# CONTAINER_IMAGE="us-docker.pkg.dev/tink-test-infrastructure/tink-ci-images/linux-tink-java-base:latest" \
+#  sh ./kokoro/gcp_ubuntu/bazel/run_tests.sh
+#
+# The user may specify TINK_BASE_DIR as the folder where to look for
+# tink-java-awskms and its depndencies. That is:
+#   ${TINK_BASE_DIR}/tink_java
+#   ${TINK_BASE_DIR}/tink_java_apps
+set -eEuo pipefail
 
 IS_KOKORO="false"
 if [[ -n "${KOKORO_ARTIFACTS_DIR:-}" ]] ; then
@@ -22,13 +33,33 @@ if [[ -n "${KOKORO_ARTIFACTS_DIR:-}" ]] ; then
 fi
 readonly IS_KOKORO
 
+RUN_COMMAND_ARGS=()
 if [[ "${IS_KOKORO}" == "true" ]] ; then
   TINK_BASE_DIR="$(echo "${KOKORO_ARTIFACTS_DIR}"/git*)"
   cd "${TINK_BASE_DIR}/tink_java_apps"
+  source "./kokoro/testutils/tink_test_container_images.sh"
+  CONTAINER_IMAGE="${TINK_JAVA_BASE_IMAGE}"
+  RUN_COMMAND_ARGS+=( -k "${TINK_GCR_SERVICE_KEY}" )
 fi
+: "${TINK_BASE_DIR:=$(cd .. && pwd)}"
+readonly TINK_BASE_DIR
+readonly CONTAINER_IMAGE
+
+cp WORKSPACE WORKSPACE.bak
 
 ./kokoro/testutils/replace_http_archive_with_local_repository.py \
-  -f "WORKSPACE" -t "${TINK_BASE_DIR}"
+  -f "WORKSPACE" -t ..
+
+if [[ -n "${CONTAINER_IMAGE:-}" ]]; then
+  RUN_COMMAND_ARGS+=( -c "${CONTAINER_IMAGE}" )
+fi
+
+# Run cleanup on EXIT.
+trap cleanup EXIT
+
+cleanup() {
+  mv WORKSPACE.bak WORKSPACE
+}
 
 # TODO(b/263465812): Add test example app for testing the Maven snapshots.
 
@@ -40,15 +71,26 @@ if [[ "${IS_KOKORO}" == "true" \
   readonly GIT_CREDENTIALS="ise-crypto:${GITHUB_ACCESS_TOKEN}"
   readonly GITHUB_URL="https://${GIT_CREDENTIALS}@github.com/tink-crypto/tink-java-apps.git"
 
-  ./maven/maven_deploy_library.sh -u "${GITHUB_URL}" \
-    -n paymentmethodtoken/maven snapshot apps-paymentmethodtoken \
-    maven/tink-java-apps-paymentmethodtoken.pom.xml HEAD
+  # Share the required env variables with the container to allow publishing the
+  # snapshot on Sonatype.
+  cat <<EOF > env_variables.txt
+SONATYPE_USERNAME
+SONATYPE_PASSWORD
+EOF
+  RUN_COMMAND_ARGS+=( -e env_variables.txt )
 
-  ./maven/maven_deploy_library.sh -u "${GITHUB_URL}" \
-    -n rewardedads/maven snapshot apps-rewardedads \
-    maven/tink-java-apps-rewardedads.pom.xml HEAD
+  ./kokoro/testutils/run_command.sh "${RUN_COMMAND_ARGS[@]}" \
+    ./maven/maven_deploy_library.sh -u "${GITHUB_URL}" \
+      -n paymentmethodtoken/maven snapshot apps-paymentmethodtoken \
+      maven/tink-java-apps-paymentmethodtoken.pom.xml HEAD
 
-  ./maven/maven_deploy_library.sh -u "${GITHUB_URL}" \
-    -n webpush/maven snapshot apps-webpush \
-    maven/tink-java-apps-webpush.pom.xml HEAD
+  ./kokoro/testutils/run_command.sh "${RUN_COMMAND_ARGS[@]}" \
+    ./maven/maven_deploy_library.sh -u "${GITHUB_URL}" \
+      -n rewardedads/maven snapshot apps-rewardedads \
+      maven/tink-java-apps-rewardedads.pom.xml HEAD
+
+  ./kokoro/testutils/run_command.sh "${RUN_COMMAND_ARGS[@]}" \
+    ./maven/maven_deploy_library.sh -u "${GITHUB_URL}" \
+      -n webpush/maven snapshot apps-webpush \
+      maven/tink-java-apps-webpush.pom.xml HEAD
 fi
