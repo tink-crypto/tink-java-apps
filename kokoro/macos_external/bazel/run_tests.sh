de7954f145378eb39848be8c3a66c353982bcff8
Moreno Ambrosin <ambrosin@google.com>
2024-07-02 05:26:26 -0700

    Install a more recent version of the Android cmdline-tools in macOS tests
    PiperOrigin-RevId: 648683213
    Change-Id: I56046439e1bbfb992bb6ca3ae2d39b95bdeea25e

--- kokoro/macos_external/bazel/run_tests.sh
+++ kokoro/macos_external/bazel/run_tests.sh
@@ -16,16 +16,13 @@
 
 set -euo pipefail
 
-export XCODE_VERSION="14.1"
-export DEVELOPER_DIR="/Applications/Xcode.app/Contents/Developer"
-export ANDROID_HOME="/usr/local/share/android-sdk"
-export COURSIER_OPTS="-Djava.net.preferIPv6Addresses=true"
-
 if [[ -n "${KOKORO_ROOT:-}" ]] ; then
   readonly TINK_BASE_DIR="$(echo "${KOKORO_ARTIFACTS_DIR}"/git*)"
   cd "${TINK_BASE_DIR}/tink_java_apps"
   export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk-8-latest/Contents/Home
+  export XCODE_VERSION="14.1"
+  export DEVELOPER_DIR="/Applications/Xcode.app/Contents/Developer"
 fi
 
-./kokoro/testutils/update_android_sdk.sh
+source ./kokoro/testutils/update_android_sdk.sh
 ./kokoro/testutils/run_bazel_tests.sh .
