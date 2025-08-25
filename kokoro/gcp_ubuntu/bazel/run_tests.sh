16ba0e826de0394dedffea4af21b0e65d29c6245
Thomas Holenstein <tholenst@google.com>
2025-08-25 01:17:26 -0700

    Make the "tink_java_apps" script self contained.
    PiperOrigin-RevId: 799014403
    Change-Id: Ie91f1eabe45f47a9a4d3c7583f84e2247568012f

--- kokoro/gcp_ubuntu/bazel/run_tests.sh
+++ kokoro/gcp_ubuntu/bazel/run_tests.sh
@@ -31,22 +31,92 @@ echo "==========================================================================
 echo "Tink Script ID: d07f521e94e09d66b551 (to quickly find the script from logs)"
 echo "==========================================================================="
 
-set -eEuo pipefail
+set -eEuox pipefail
 
-RUN_COMMAND_ARGS=()
-if [[ -n "${KOKORO_ARTIFACTS_DIR:-}" ]] ; then
-  readonly TINK_BASE_DIR="$(echo "${KOKORO_ARTIFACTS_DIR}"/git*)"
-  cd "${TINK_BASE_DIR}/tink_java_apps"
-  source ./kokoro/testutils/java_test_container_images.sh
+if [[ ! -v TINK_BASE_DIR ]] ; then
+  TINK_BASE_DIR="$(echo "${KOKORO_ARTIFACTS_DIR}"/git*)"
+fi
+readonly TINK_BASE_DIR
+echo "TINK_BASE_DIR is ${TINK_BASE_DIR}"
+if [[ ! -v CONTAINER_IMAGE ]] ; then
+  source \
+    "${TINK_BASE_DIR}/tink_java_apps/kokoro/testutils/java_test_container_images.sh"
   CONTAINER_IMAGE="${TINK_JAVA_BASE_IMAGE}"
-  RUN_COMMAND_ARGS+=( -k "${TINK_GCR_SERVICE_KEY}" )
 fi
 readonly CONTAINER_IMAGE
+echo "CONTAINER_IMAGE is ${CONTAINER_IMAGE}"
 
-if [[ -n "${CONTAINER_IMAGE:-}" ]]; then
-  RUN_COMMAND_ARGS+=( -c "${CONTAINER_IMAGE}" )
+cd "${TINK_BASE_DIR}/tink_java_apps"
+
+if [[ -v TINK_REMOTE_BAZEL_CACHE_GCS_BUCKET ]]; then
+  cp "${TINK_REMOTE_BAZEL_CACHE_SERVICE_KEY}" ./cache_key
+  cat <<EOF > /tmp/env_variables.txt
+BAZEL_REMOTE_CACHE_NAME=${TINK_REMOTE_BAZEL_CACHE_GCS_BUCKET}/bazel/${TINK_JAVA_BASE_IMAGE}
+EOF
+else
+  touch /tmp/env_variables.txt
 fi
-readonly RUN_COMMAND_ARGS
 
-./kokoro/testutils/docker_execute.sh "${RUN_COMMAND_ARGS[@]}" \
-  ./kokoro/testutils/run_bazel_tests.sh .
+
+### =========================================== EMBEDDED SCRIPT: _do_run_test.sh
+### The _do_run_test.sh script will run as "tinkuser"
+cat <<'EOF' > _do_run_test.sh
+#!/bin/bash
+set -euo pipefail
+
+cd /home/tinkuser/tink_java_apps/
+
+CACHE_FLAGS=()
+if [[ -n "${BAZEL_REMOTE_CACHE_NAME:-}" ]]; then
+  CACHE_FLAGS+=("--remote_cache=https://storage.googleapis.com/${BAZEL_REMOTE_CACHE_NAME}")
+  CACHE_FLAGS+=("--google_credentials=$(realpath ./cache_key)")
+fi
+readonly CACHE_FLAGS
+
+cat .bazelrc
+
+echo "---------- BUILDING MAIN"
+time bazelisk build "${CACHE_FLAGS[@]}" -- ...
+echo "---------- TESTING MAIN"
+time bazelisk test "${CACHE_FLAGS[@]}" -- ...
+
+EOF
+### ======================================================= END: _do_run_test.sh
+
+chmod +x _do_run_test.sh
+
+### =========================== EMBEDDED SCRIPT: _cp_switch_user_and_run_test.sh
+### The _cp_switch_user_and_run_test.sh script will cp the build tree to a
+### directory readable for tinkuser then execute _do_run_test.sh.
+cat <<'EOF' > _cp_switch_user_and_run_test.sh
+set -euo pipefail
+
+echo "=== COPYING /tink_orig_dir to /home/tinkuser"
+
+cp -r /tink_orig_dir/tink_java_apps /home/tinkuser
+chown --recursive tinkuser:tinkgroup /home/tinkuser/tink_java_apps
+
+su tinkuser /usr/bin/bash -c "/home/tinkuser/tink_java_apps/_do_run_test.sh"
+exit
+EOF
+### ======================================= END: _cp_switch_user_and_run_test.sh
+
+chmod +x _cp_switch_user_and_run_test.sh
+
+if [[ ! -z "${TINK_GCR_SERVICE_KEY:-}" ]]; then
+  gcloud auth activate-service-account --key-file="${TINK_GCR_SERVICE_KEY}"
+  gcloud config set project tink-test-infrastructure
+  gcloud auth configure-docker us-docker.pkg.dev --quiet
+fi
+
+echo "-- PULLING DOCKER IMAGE"
+time docker pull "${CONTAINER_IMAGE}"
+
+echo "-- RUNNING DOCKER"
+time docker run \
+  --network="host" \
+  --mount type=bind,src="${TINK_BASE_DIR}",dst=/tink_orig_dir \
+  --env-file /tmp/env_variables.txt \
+  --rm \
+  "${CONTAINER_IMAGE}" \
+  bash -c "/tink_orig_dir/tink_java_apps/_cp_switch_user_and_run_test.sh"
